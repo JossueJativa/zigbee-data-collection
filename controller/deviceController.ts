433293997f572d220ac8b965bc8c764f966e6fe2
Jossue <jativatamayojossuedavid@gmail.com>
2024-12-09 10:41:02 -0500

    Pruebas de concepto

--- controller/deviceController.ts
+++ controller/deviceController.ts
@@ -3,7 +3,37 @@ import axios from "axios";
 import { baseUrl } from "./config/dataService";
 import { enableDongle } from "./dongleController";
 
+const getDevices = async (): Promise<string[]> => {
+    try {
+        const response = await axios.get(`${baseUrl}/devices`);
+        const devices = response.data;
+        return devices;
+    } catch (error) {
+        return [ `${error}` ]
+    }
+}
+
+const getResponseDevicesMac = async (macAddress: string): Promise<Record<string, any>> => {
+    try {
+        const response = await axios.get(`${baseUrl}/devices/${macAddress}`);
+        const device = response.data;
+        return device;
+    } catch (error) {
+        return {
+            "error": error
+        }
+    }
+}
+
 const getSensors = async (): Promise<Record<string, ZigBeeDevice>> => {
+    
+    const devicesPrint = await getDevices();
+    for (const deviceMac of devicesPrint) {
+        const deviceDetails = await getResponseDevicesMac(deviceMac);
+        console.log(`Detalles del dispositivo con MAC ${deviceMac}:`);
+        console.log(JSON.stringify(deviceDetails, null, 2));
+    }
+
     try {
         const response = await axios.get(`${baseUrl}/sensors`);
         const sensors = response.data;
