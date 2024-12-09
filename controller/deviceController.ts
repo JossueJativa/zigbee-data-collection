a944442122b59831707ed218c274d9aad455d89a
Jossue <jativatamayojossuedavid@gmail.com>
2024-12-09 13:24:21 -0500

    Update to get Gas Sensor

--- controller/deviceController.ts
+++ controller/deviceController.ts
@@ -15,12 +15,27 @@ const getSensors = async (): Promise<Record<string, ZigBeeDevice>> => {
     }
 }
 
+const getGasDetector = async (): Promise<Record<string, ZigBeeDevice>> => {
+    try {
+        const response = await axios.get(`${baseUrl}/lights`);
+        const lights = response.data;
+        return lights;
+    } catch (error) {
+        return {
+            "error": error as unknown as ZigBeeDevice
+        }
+    }
+}
+
 const addNewDevice = async (panicButtonsIds: string[]): Promise<void> => {
     await enableDongle();
 
     setTimeout(
         async () => {
             const sensors = await getSensors();
+            const lights = await getGasDetector();
+
+            console.log("lights: ", lights);
             
             for (const sensor of Object.values(sensors)) {
                 if (sensor.type === "ZHASwitch" && !panicButtonsIds.includes(sensor.id)) {
